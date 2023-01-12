61b9e4dc94c8648da7da5a7d7453659d99757c3f
Gary-Ihar <yakuzawot6@gmail.com>
2023-01-12 20:58:03 +0300

    rf env

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,8 @@
 /// <reference types="react-scripts" />
-declare module "*.svg";
-declare module "*.png";
-declare module "*.jpg";
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
