f63c5350bfb35ba114dfdac6caf2474e83f8c666
Gary-Ihar <yakuzawot6@gmail.com>
2023-01-12 20:16:57 +0300

    init draft env

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { observer } from 'mobx-react';
 
-function App() {
+const App = observer(function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>Do action!</span>
     </div>
   );
-}
+});
 
 export default App;
