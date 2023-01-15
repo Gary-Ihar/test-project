f02e49b3fb4eb01bf25b59ad0d6a324369efcc39
Gary-Ihar <yakuzawot6@gmail.com>
2023-01-15 13:33:16 +0300

    add animation to bg circles

--- src/components/Layout/components/BgCircle.tsx
+++ src/components/Layout/components/BgCircle.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import classNames from 'classnames';
 import style from './style.module.css';
+import animatedStyle from './animated.module.css';
 
 type Props = {
   position?: 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight';
   color?: 'white' | 'green';
+  animated?: boolean;
 };
 
-export const BgCircle = ({ position = 'topLeft', color = 'white' }: Props) => {
+export const BgCircle = ({ position = 'topLeft', color = 'white', animated = true }: Props) => {
   const className = classNames(style.bgCircle, {
     [style[position]]: position,
     [style[color]]: color,
+    [animatedStyle[position]]: animated,
   });
   return <div className={className} />;
 };
